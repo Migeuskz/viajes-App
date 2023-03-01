0f2be5048f2f9e54093b6ab462cc1d664582eb72
Migeuskz <msanchezpapalotzi@gmail.com>
2023-03-01 11:45:09 -0600

    Agregando Gradientes 1

---
+++ lib/screens/gradient_back.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GradientBack extends StatelessWidget {
+  const GradientBack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250.0,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(0, 2, 2, 2),
+            Color.fromARGB(255, 89, 65, 55),
+          ],
+          begin: FractionalOffset(0.2, 0.0),
+          end: FractionalOffset(1.0, 0.6),
+          stops: [0.0, 0.6],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+    );
+  }
+}
